9ff0e7a4e10f021c99dca85cd3910f966c55a0a0
ilyasaruca <ilyasaruca@gmail.com>
2019-02-21 14:34:52 +0300

    Encode-DecodeWithString
    Here is a code snippet to help you encrypt and decrypt a string in C#. The key is hardcoded, but in your own project you may want to read it from a secure store, or perhaps a config file or environment variable. Notice that the key is being hashed for an extra layer of security.

---
+++ Encode-DecodeWithString/MD5.cs
@@ -0,0 +1,51 @@
+﻿using System; 
+using System.Security.Cryptography;
+using System.Text; 
+
+namespace Encode_DecodeWithString
+{
+    public static class MD5
+    {
+        private static string key { get; set; } = "v+4Ye9K_6M4Qf@9J";
+
+        public static string Encrypt(string data)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                using (var tdes = new TripleDESCryptoServiceProvider())
+                {
+                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    using (var transform = tdes.CreateEncryptor())
+                    {
+                        byte[] textBytes = UTF8Encoding.UTF8.GetBytes(data);
+                        byte[] bytes = transform.TransformFinalBlock(textBytes, 0, textBytes.Length);
+                        return Convert.ToBase64String(bytes, 0, bytes.Length);
+                    }
+                }
+            }
+        }
+
+        public static string Decrypt(string encryptData)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                using (var tdes = new TripleDESCryptoServiceProvider())
+                {
+                    tdes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    using (var transform = tdes.CreateDecryptor())
+                    {
+                        byte[] cipherBytes = Convert.FromBase64String(encryptData);
+                        byte[] bytes = transform.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+                        return UTF8Encoding.UTF8.GetString(bytes);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Encode-DecodeWithString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encode_DecodeWithString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "3myna7kz3q7j";
+            Console.WriteLine("Original Password : {0}", string.Format(password));
+
+            var encrypt = MD5.Encrypt(password);
+            Console.WriteLine("Encrypt Password  : {0}", string.Format(encrypt));
+
+            var decrypt = MD5.Decrypt(encrypt);
+            Console.WriteLine("Decrypt Password  : {0}", string.Format(decrypt));
+
+            Console.ReadKey();
+        }
+    }
+}
